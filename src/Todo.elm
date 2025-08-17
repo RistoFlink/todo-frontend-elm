ef7b369f31fa69a41e17dabd8eb91213803e47a4
RistoFlink <kenssi@gmail.com>
2025-08-17 18:47:07 +0300

    Implement some decoders

--- src/Todo.elm
+++ src/Todo.elm
@@ -4,7 +4,7 @@ import Json.Decode as Decode exposing (Decoder)
 import Json.Encode as Encode
 import Time
 
-
+-- Types
 
 type Priority
     = High
@@ -54,3 +54,34 @@ type alias CompletionStats =
     , incomple : Int
     , completionTotal : Int
     }
+
+-- JSON decoders
+priorityDecoder : Decoder Priority
+priorityDecoder =
+    Decode.string |> Decode.andThen (\str ->
+        case str of
+            "High" -> Decode.succeed High
+            "Medium" -> Decode.succeed Medium
+            "Low" -> Decode.succeed Low
+            _ -> Decode.fail ("Unknown priority: " ++ str)
+    )
+
+todoDecoder : Decoder Todo
+todoDecoder =
+    Decode.map7 Todo
+        (Decode.field "id" Decode.int)
+        (Decode.field "title" Decode.string)
+        (Decode.field "completed" Decode.bool)
+        (Decode.field "createdAt" (Decode.map Time.millisToPosix Decode.int))
+        (Decode.field "updatedAt" (Decode.map Time.millisToPosix Decode.int))
+        (Decode.field "dueDate" (Decode.nullable (Decode.map Time.millisToPosix Decode.int)))
+        (Decode.field "priority" priorityDecoder)
+
+
+-- Helper functions
+priorityToString : Priority -> String
+priorityToString priority =
+    case priority of
+        High -> "High"
+        Medium -> "Medium"
+        Low -> "Low"
